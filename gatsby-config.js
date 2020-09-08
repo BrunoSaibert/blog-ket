c916613c67fafe6d091f428c2050eb7163b3e749
BrunoSaibert <brunosaibert@gmail.com>
2020-09-07 21:02:03 -0300

    feat: cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/static/images/upload`,
+        name: `upload`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
